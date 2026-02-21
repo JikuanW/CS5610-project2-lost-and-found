a32022a2ff0d971e82c8950e37cdf296cb5dbe46
SoniRusagara <soni.rusagara@gmail.com>
2026-02-21 09:19:12 -0500

    Update nav.js

--- public/js/nav.js
+++ public/js/nav.js
@@ -46,8 +46,11 @@ async function renderNav() {
           <a href="/found-my.html">My Found</a>
           <span class="muted">•</span> 
           <a href="/lost-my.html">My Lost</a>
-          <span class="muted">•</span> 
-          ${me.role === "admin" ? `<a href="/admin.html">Admin</a>` : ""}
+          ${me.role === "admin"
+            ? `<span class="muted">•</span>
+               <a href="/admin.html">Admin</a>`
+            : ""}
+          
         </div>
       </div>
     </div>
